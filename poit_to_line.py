9e09ef24a74d6397b518f8eb53ec106d8543f456
Thais Sousa <64991864+psiques@users.noreply.github.com>
2023-06-12 07:01:33 -0300

    Update poit_to_line.py

--- poit_to_line.py
+++ poit_to_line.py
@@ -2,46 +2,46 @@ import geopandas as gpd
 from shapely.geometry import LineString
 
 # Função para recortar o shapefile de entrada pelo buffer
-def recortar_shapefile(buffer, shapefile):
-    recorte = gpd.clip(shapefile, buffer)
+def recortar_shape(buffer, shape):
+    recorte = gpd.clip(shape, buffer)
     return recorte
 
 # Função para salvar o GeoDataFrame como shapefile
-def salvar_shapefile(gdf, caminho):
+def salvar_shape(gdf, caminho):
     gdf.to_file(caminho)
 
 # Caminho para o arquivo de pontos
-cam_pontos = input("Insira o caminho para o arquivo de pontos: ")
+cam = input("Insira o caminho para o arquivo de pontos: ")
 
 # Substituindo as barras invertidas por barras normais
-cam_pontos = cam_pontos.replace("\\", "/")
+cam = cam.replace("\\", "/")
 
 # Carregando o shapefile de pontos
-pontos = gpd.read_file(cam_pontos)
+pontos = gpd.read_file(cam)
 
 # Calculando a média da distância entre os pontos
-distancias = []
+dists = []
 for i in range(len(pontos)-1):
-    ponto_atual = pontos.iloc[i]
-    proximo_ponto = pontos.iloc[i+1]
-    distancia = ponto_atual.geometry.distance(proximo_ponto.geometry)
-    distancias.append(distancia)
+    atual = pontos.iloc[i]
+    prox = pontos.iloc[i+1]
+    dist = atual.geometry.distance(prox.geometry)
+    dists.append(dist)
 
-media_distancia = sum(distancias) / len(distancias)
-limite_distancia = 3 * media_distancia
+media_dist = sum(dists) / len(dists)
+limite_dist = 3 * media_dist
 
 # Removendo os segmentos de linha que ultrapassem o limite de distância
-segmentos_linha = []
+segmentos = []
 for i in range(len(pontos)-1):
-    ponto_atual = pontos.iloc[i]
-    proximo_ponto = pontos.iloc[i+1]
-    distancia = ponto_atual.geometry.distance(proximo_ponto.geometry)
-    if distancia <= limite_distancia:
-        segmento_linha = LineString([ponto_atual.geometry, proximo_ponto.geometry])
-        segmentos_linha.append(segmento_linha)
+    atual = pontos.iloc[i]
+    prox = pontos.iloc[i+1]
+    dist = atual.geometry.distance(prox.geometry)
+    if dist <= limite_dist:
+        segmento = LineString([atual.geometry, prox.geometry])
+        segmentos.append(segmento)
 
 # Criando um GeoDataFrame com os segmentos de linha
-linhas = gpd.GeoDataFrame(geometry=segmentos_linha, crs='EPSG:32723')
+linhas = gpd.GeoDataFrame(geometry=segmentos, crs='EPSG:32723')
 
 # Solicitando a metragem do buffer em metros
 buffer_size = float(input("Digite a metragem do buffer em metros: "))
@@ -50,38 +50,38 @@ buffer_size = float(input("Digite a metragem do buffer em metros: "))
 areas_buffer = linhas.buffer(buffer_size)
 
 # Dissolvendo as áreas
-area_dissolvida = areas_buffer.unary_union
+area_dissolv = areas_buffer.unary_union
 
 # Criando um GeoDataFrame com a área dissolvida
-area_dissolvida_gdf = gpd.GeoDataFrame(geometry=[area_dissolvida], crs='EPSG:32723')
+area_dissolv_gdf = gpd.GeoDataFrame(geometry=[area_dissolv], crs='EPSG:32723')
 
 # Caminho para o shapefile de entrada para recortar
-caminho_shapefile = input("Insira o caminho para o shapefile a ser recortado: ")
+caminho_shape = input("Insira o caminho para o shapefile a ser recortado: ")
 
 # Substituindo as barras invertidas por barras normais
-caminho_shapefile = caminho_shapefile.replace("\\", "/")
+caminho_shape = caminho_shape.replace("\\", "/")
 
 # Carregando o shapefile a ser recortado
-shapefile_recortar = gpd.read_file(caminho_shapefile)
+shape_recort = gpd.read_file(caminho_shape)
 
 # Recortando o shapefile pelo buffer
-shapefile_recortado = recortar_shapefile(area_dissolvida_gdf.geometry[0], shapefile_recortar)
+shape_recortado = recortar_shape(area_dissolv_gdf.geometry[0], shape_recort)
 
 # Salvando os shapefiles de saída
-caminho_saida = input("Digite o diretório de saída: ")
+saida = input("Digite o diretório de saída: ")
 
 # Perguntar ao usuário se deseja salvar a trilha
 salvar_trilha = input("Deseja salvar a trilha? (S/N): ")
 if salvar_trilha.upper() == "S":
-    trilha_saida = caminho_saida + "/trilha_" + cam_pontos.split("/")[-1]
-    salvar_shapefile(linhas, trilha_saida)
+    trilha_saida = saida + "/trilha_" + cam.split("/")[-1]
+    salvar_shape(linhas, trilha_saida)
 
 # Perguntar ao usuário se deseja salvar o buffer
 salvar_buffer = input("Deseja salvar o buffer? (S/N): ")
 if salvar_buffer.upper() == "S":
-    buffer_saida = caminho_saida + "/buffer_" + str(buffer_size) + "m"
-    salvar_shapefile(area_dissolvida_gdf, buffer_saida)
+    buffer_saida = saida + "/buffer_" + str(buffer_size) + "m"
+    salvar_shape(area_dissolv_gdf, buffer_saida)
 
 # Salvando o shapefile recortado
-recorte_saida = caminho_saida + "/recorte_" + caminho_shapefile.split("/")[-1]
-salvar_shapefile(shapefile_recortado, recorte_saida)
+recorte_saida = saida + "/recorte_" + caminho_shape.split("/")[-1]
+salvar_shape(shape_recortado, recorte_saida)
